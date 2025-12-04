b874726f200bc4fe4c68ff4a034eb91367a6ec13
Артём М <pers.kg@gmail.com>
2025-12-04 23:17:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3308346514-71752",
+  "appId": "1:705530909842:web:f455df74e658e79998824b",
+  "apiKey": "AIzaSyD8afR8TffZW4AjQX4IckRP51ShLjx57N0",
+  "authDomain": "studio-3308346514-71752.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "705530909842"
+};
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,54 +1,50 @@
-import 'dotenv/config'
-import {
-  FirebaseApp,
-  FirebaseOptions,
-  getApp,
-  getApps,
-  initializeApp,
-} from 'firebase/app';
-import { Auth, getAuth, connectAuthEmulator } from 'firebase/auth';
-import {
-  Firestore,
-  getFirestore,
-  connectFirestoreEmulator,
-} from 'firebase/firestore';
+'use client';
 
-let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
+import { firebaseConfig } from '@/firebase/config';
+import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
-
-function initializeFirebase() {
-  if (getApps().length === 0) {
-    firebaseApp = initializeApp(firebaseConfig);
-    auth = getAuth(firebaseApp);
-    firestore = getFirestore(firebaseApp);
-
-    if (process.env.NODE_ENV === 'development') {
-      // Point to the emulators running on localhost.
-      // Make sure you're running the emulators with `firebase emulators:start`
-      // connectAuthEmulator(auth, 'http://localhost:9099', {
-      //   disableWarnings: true,
-      // });
-      // connectFirestoreEmulator(firestore, 'localhost', 8080);
+// IMPORTANT: DO NOT MODIFY THIS FUNCTION
+export function initializeFirebase() {
+  if (!getApps().length) {
+    // Important! initializeApp() is called without any arguments because Firebase App Hosting
+    // integrates with the initializeApp() function to provide the environment variables needed to
+    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
+    // without arguments.
+    let firebaseApp;
+    try {
+      // Attempt to initialize via Firebase App Hosting environment variables
+      firebaseApp = initializeApp();
+    } catch (e) {
+      // Only warn in production because it's normal to use the firebaseConfig to initialize
+      // during development
+      if (process.env.NODE_ENV === "production") {
+        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
+      }
+      firebaseApp = initializeApp(firebaseConfig);
     }
-  } else {
-    firebaseApp = getApp();
-    auth = getAuth(firebaseApp);
-    firestore = getFirestore(firebaseApp);
+
+    return getSdks(firebaseApp);
   }
-  return { firebaseApp, auth, firestore };
+
+  // If already initialized, return the SDKs with the already initialized App
+  return getSdks(getApp());
 }
 
-export { initializeFirebase };
+export function getSdks(firebaseApp: FirebaseApp) {
+  return {
+    firebaseApp,
+    auth: getAuth(firebaseApp),
+    firestore: getFirestore(firebaseApp)
+  };
+}
 
 export * from './provider';
+export * from './client-provider';
 export * from './firestore/use-collection';
+export * from './firestore/use-doc';
+export * from './non-blocking-updates';
+export * from './non-blocking-login';
+export * from './errors';
+export * from './error-emitter';
